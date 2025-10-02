2d05bd9d25f229a69453c988f8a25e9ec2459fae
OsamaRab3 <osrab3@gmail.com>
2025-10-02 14:44:41 +0300

    setup vercel for deployment
    Signed-off-by: OsamaRab3 <osrab3@gmail.com>

--- src/config/environment.js
+++ src/config/environment.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 
 module.exports = {
     PORT: process.env.PORT || 3000,
-    DB_URL: process.env.DB_URL || 'Your DB_URL',
+    DB_URL: process.env.MYSQL_URL || 'Your DB_URL',
     JWT_SECRET: process.env.JWT_SECRET || 'your_secret_key',
     expiresIn: process.env.expiresIn,
 

8d32066a29e47d6ef56451db1c78869ddaa97bc0
OsamaRab3 <osrab3@gmail.com>
2025-10-04 15:12:41 +0300

    Add: province controller and routes

---
+++ src/api/routes/provinceRoutes.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const router = express.Router();
+const provinceController = require('../controllers/provinceController');
+const verifyToken = require('../../middleware/verifyToken');
+const allowTo = require('../../middleware/allowTo');
+
+const {
+  createProvinceValidation,
+  updateProvinceValidation,
+  provinceIdValidation,
+  updateDeliveryFeeValidation,
+  removeDeliveryFeeValidation
+} = require('../../validation/province.validation');
+
+router.get('/', provinceController.getAllProvinces);
+router.get('/:id', provinceIdValidation, provinceController.getProvinceById);
+
+
+router.post(
+  '/',
+  verifyToken,
+  allowTo('ADMIN'),
+  createProvinceValidation,
+  provinceController.createProvince
+);
+
+router.put(
+  '/:id',
+  verifyToken,
+  allowTo('ADMIN'),
+  updateProvinceValidation,
+  provinceController.updateProvince
+);
+
+router.delete(
+  '/:id',
+  verifyToken,
+  allowTo('ADMIN'),
+  provinceIdValidation,
+  provinceController.deleteProvince
+);
+
+
+router.post(
+  '/:provinceId/products/:productId/delivery-fee',
+  verifyToken,
+  allowTo('ADMIN'),
+  updateDeliveryFeeValidation,
+  provinceController.updateDeliveryFee
+);
+
+router.delete(
+  '/:provinceId/products/:productId/delivery-fee',
+  verifyToken,
+  allowTo('ADMIN'),
+  removeDeliveryFeeValidation,
+  provinceController.removeDeliveryFee
+);
+
+module.exports = router;
